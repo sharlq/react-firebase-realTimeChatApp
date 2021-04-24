eb833b769bd9c4c435cdc9f7b237b440f0f25d65
sharlq <shehabakhader@gmail.com>
2021-04-24 12:12:12 +0300

    the project is done now its the time to sryle it and orgnized it more

--- src/components/channel.jsx
+++ src/components/channel.jsx
@@ -1,13 +1,13 @@
 import React, {useState,useEffect} from "react"
 import firebase from 'firebase/app';
+import Message from './message'
 
 const Channel = ({user = null, db = null}) => {
     const [messages,setMessages] = useState([]);
-    console.log(db)
+    const [newMessage,setNewMessage]=useState();
+    const {uid , displayName, photoURL} = user;
     useEffect(() => {
-        console.log("out")
        if(db){
-        console.log("in")
        const unsubscribe = db
        .collection('messages')
        .orderBy('createdAt')
@@ -22,25 +22,47 @@ const Channel = ({user = null, db = null}) => {
            }));
            //Update state
            setMessages(data);
+           // note the problem here was that i didnt give the server teh preimition to reade adn write data from firebases
        });
-
-
-       
        return unsubscribe;
-       
+    }
+    
+    }, [db])
+
+
+    const handleOnChange = (e) =>{
+            setNewMessage(e.target.value)
+    }
+
+    const handleOnSubmit = (e) =>{
+        e.preventDefault();
+        if(db){
+            db.collection("messages").add({
+                text: newMessage,
+                createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+                uid,
+                displayName,
+                photoURL
+            })
+        }
+        setNewMessage("")
     }
     console.log(messages)
-    }, [db])
-    console.log(messages)
     return (
+        <>
         <ul>
            {messages.map(message => (
 
-               <li key={message.id}>{message.text}</li>
+               <li key={message.id}> <Message {...message}/> </li>
            ))
            
            } 
         </ul>
+        <form onSubmit={(e)=>handleOnSubmit(e)} >
+            <input type="text" value={newMessage} onChange={(e)=>handleOnChange(e)} />
+            <button>send message</button>
+        </form>
+        </>
     )
 }
 
